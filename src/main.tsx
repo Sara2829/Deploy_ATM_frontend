40df465d48585d517a2caf5a2bc786688515126d
Sara2829 <2021bec102@sggs.ac.in>
2025-01-16 16:35:20 +0530

    testing main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,52 +1,58 @@
-import {createRoot} from 'react-dom/client'
-// Axios
-import axios from 'axios'
-import {Chart, registerables} from 'chart.js'
-import {QueryClient, QueryClientProvider} from 'react-query'
-import {ReactQueryDevtools} from 'react-query/devtools'
-// Apps
-import {MetronicI18nProvider} from './_metronic/i18n/Metronici18n'
-import './_metronic/assets/sass/style.react.scss'
-import './_metronic/assets/fonticon/fonticon.css'
-import './_metronic/assets/keenicons/duotone/style.css'
-import './_metronic/assets/keenicons/outline/style.css'
-import './_metronic/assets/keenicons/solid/style.css'
-/**
- * TIP: Replace this style import with rtl styles to enable rtl mode
- *
- * import './_metronic/assets/css/style.rtl.css'
- **/
-import './_metronic/assets/sass/style.scss'
-import {AppRoutes} from './app/routing/AppRoutes'
-import {AuthProvider, setupAxios} from './app/modules/auth'
-/**
- * Creates `axios-mock-adapter` instance for provided `axios` instance, add
- * basic Metronic mocks and returns it.
- *
- * @see https://github.com/ctimmerm/axios-mock-adapter
- */
-/**
- * Inject Metronic interceptors for axios.
- *
- * @see https://github.com/axios/axios#interceptors
- */
-setupAxios(axios)
-Chart.register(...registerables)
+// import {createRoot} from 'react-dom/client'
+// // Axios
+// import axios from 'axios'
+// import {Chart, registerables} from 'chart.js'
+// import {QueryClient, QueryClientProvider} from 'react-query'
+// import {ReactQueryDevtools} from 'react-query/devtools'
+// // Apps
+// import {MetronicI18nProvider} from './_metronic/i18n/Metronici18n'
+// import './_metronic/assets/sass/style.react.scss'
+// import './_metronic/assets/fonticon/fonticon.css'
+// import './_metronic/assets/keenicons/duotone/style.css'
+// import './_metronic/assets/keenicons/outline/style.css'
+// import './_metronic/assets/keenicons/solid/style.css'
+// /**
+//  * TIP: Replace this style import with rtl styles to enable rtl mode
+//  *
+//  * import './_metronic/assets/css/style.rtl.css'
+//  **/
+// import './_metronic/assets/sass/style.scss'
+// import {AppRoutes} from './app/routing/AppRoutes'
+// import {AuthProvider, setupAxios} from './app/modules/auth'
+// /**
+//  * Creates `axios-mock-adapter` instance for provided `axios` instance, add
+//  * basic Metronic mocks and returns it.
+//  *
+//  * @see https://github.com/ctimmerm/axios-mock-adapter
+//  */
+// /**
+//  * Inject Metronic interceptors for axios.
+//  *
+//  * @see https://github.com/axios/axios#interceptors
+//  */
+// setupAxios(axios)
+// Chart.register(...registerables)
 
-const queryClient = new QueryClient()
-const container = document.getElementById('root')
-if (container) {
-  createRoot(container).render(
-    <QueryClientProvider client={queryClient}>
-      <h1>Entered to Qurey client</h1>
-      <MetronicI18nProvider>
-        <h1>Entered to MetronicProvider</h1>
-        <AuthProvider>
-          <h1>Entered to Auth Provider</h1>
-          <AppRoutes />
-        </AuthProvider>
-      </MetronicI18nProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+// const queryClient = new QueryClient()
+// const container = document.getElementById('root')
+// if (container) {
+//   createRoot(container).render(
+//     <QueryClientProvider client={queryClient}>
+//       <MetronicI18nProvider>
+//         <AuthProvider>
+//           <AppRoutes />
+//         </AuthProvider>
+//       </MetronicI18nProvider>
+//       <ReactQueryDevtools initialIsOpen={false} />
+//     </QueryClientProvider>
+//   )
+// }
+
+
+import React from 'react'
+
+export const main = () => {
+  return (
+    <div>Rendered main.tsx</div>
   )
 }
