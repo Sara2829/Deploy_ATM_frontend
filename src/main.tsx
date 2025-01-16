d1ada08b302322b60e5d965ff19d58aecf9563bb
Sara2829 <2021bec102@sggs.ac.in>
2025-01-16 16:42:36 +0530

    added debug component

--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,18 @@
-// import {createRoot} from 'react-dom/client'
-// // Axios
-// import axios from 'axios'
-// import {Chart, registerables} from 'chart.js'
-// import {QueryClient, QueryClientProvider} from 'react-query'
-// import {ReactQueryDevtools} from 'react-query/devtools'
-// // Apps
-// import {MetronicI18nProvider} from './_metronic/i18n/Metronici18n'
-// import './_metronic/assets/sass/style.react.scss'
-// import './_metronic/assets/fonticon/fonticon.css'
-// import './_metronic/assets/keenicons/duotone/style.css'
-// import './_metronic/assets/keenicons/outline/style.css'
-// import './_metronic/assets/keenicons/solid/style.css'
-// /**
-//  * TIP: Replace this style import with rtl styles to enable rtl mode
-//  *
-//  * import './_metronic/assets/css/style.rtl.css'
-//  **/
-// import './_metronic/assets/sass/style.scss'
-// import {AppRoutes} from './app/routing/AppRoutes'
-// import {AuthProvider, setupAxios} from './app/modules/auth'
-// /**
-//  * Creates `axios-mock-adapter` instance for provided `axios` instance, add
-//  * basic Metronic mocks and returns it.
-//  *
-//  * @see https://github.com/ctimmerm/axios-mock-adapter
-//  */
-// /**
-//  * Inject Metronic interceptors for axios.
-//  *
-//  * @see https://github.com/axios/axios#interceptors
-//  */
-// setupAxios(axios)
-// Chart.register(...registerables)
+import { createRoot } from 'react-dom/client'
 
-// const queryClient = new QueryClient()
-// const container = document.getElementById('root')
-// if (container) {
-//   createRoot(container).render(
-//     <QueryClientProvider client={queryClient}>
-//       <MetronicI18nProvider>
-//         <AuthProvider>
-//           <AppRoutes />
-//         </AuthProvider>
-//       </MetronicI18nProvider>
-//       <ReactQueryDevtools initialIsOpen={false} />
-//     </QueryClientProvider>
-//   )
-// }
-
-
-import React from 'react'
-
-export const main = () => {
+// Minimal component for testing
+const DebugComponent = () => {
   return (
-    <div>Rendered main.tsx</div>
+    <div style={{ textAlign: 'center', marginTop: '50px', fontSize: '24px', color: 'green' }}>
+      <h1>Main.tsx is rendering successfully!</h1>
+    </div>
   )
+}
+
+// Locate the root element
+const container = document.getElementById('root')
+
+// Render the debug component if the container exists
+if (container) {
+  createRoot(container).render(<DebugComponent />)
 }
