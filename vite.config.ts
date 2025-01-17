d3b26eb52ff8fcb8409be657d3d7581afb60c312
Sara2829 <2021bec102@sggs.ac.in>
2025-01-17 15:28:54 +0530

    redefined some packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/metronic8/react/demo8/",
+  base: '/metronic8/react/demo8/',
   build: {
-    chunkSizeWarningLimit: 3000,
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html', // Ensures index.html is treated as the entry
+    },
   },
 })
